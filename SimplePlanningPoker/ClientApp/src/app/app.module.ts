f207ce6e0d83d50761982a3468fc5a026a80b9c0
Marius <marius_martin@t-online.de>
2023-11-09 21:44:57 +0100

    Optimized styles for bundle size

--- SimplePlanningPoker/ClientApp/src/app/app.module.ts
+++ SimplePlanningPoker/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterModule, RouterStat
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { RoomComponent } from './room/room.component';
 import { ParticipantComponent } from './participant/participant.component';
 import { CardComponent } from './card/card.component';
@@ -34,8 +32,6 @@ const canActivateRoom: CanActivateFn = (
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     RoomComponent,
     ParticipantComponent,
     CardComponent,
